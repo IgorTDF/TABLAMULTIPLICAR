60e8eddf29aa072aa7f69542c6ea25d1c976aba9
Igor.tdf <104040266+IgorTDF@users.noreply.github.com>
2022-05-06 18:58:56 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let numero1 : number = prompt ("ingrese numero")
+let numero2 :  number = prompt ("ingrese hasta que numero")
+
+for (let i=1; i< 11 ; i ++){
+  console.log ( numero1 + "x"+ i + "=" + (numero1 * i));
